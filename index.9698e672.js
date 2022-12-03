39f35edd885fb1e586a9d70bd70a05aa97fdc86c
IMissOdeToCode <107809855+IMissOdeToCode@users.noreply.github.com>
2022-12-03 18:52:10 +0000

    Deploying to gh-pages from @ IMissOdeToCode/goit-js-hw-10@487cc0f81b783c68d280ba37e3bfd1247f0555f7 🚀

--- index.9698e672.js
+++
@@ -1,2 +0,0 @@
-fetch("https://restcountries.com/v2/name/poland?fields=name,capital,population,flags,languages").then((e=>e.json())).then((e=>{console.log(e[0].name)}));
-//# sourceMappingURL=index.9698e672.js.map
