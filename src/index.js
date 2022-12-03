487cc0f81b783c68d280ba37e3bfd1247f0555f7
Denys Konopatkin <k.denis.aa@gmail.com>
2022-12-03 20:51:43 +0200

    add lodash

--- src/index.js
+++ src/index.js
@@ -1,8 +1,16 @@
 import './css/styles.css';
+const debounce = require('lodash.debounce');
+// import debounce from 'lodash.debounce';
+
+const refs = {
+  input: document.querySelector('#search-box'),
+};
+
+console.log(refs.input);
 
 const DEBOUNCE_DELAY = 300;
-// const URL = 'https://restcountries.com/v2/name/poland';
 
+// const URL = 'https://restcountries.com/v2/name/poland';
 // prettier-ignore
 const URL = 'https://restcountries.com/v2/name/poland?fields=name,capital,population,flags,languages';
 
@@ -14,6 +22,12 @@ function fetchCountriesByName() {
       return response.json();
     })
     .then(country => {
-      console.log(country[0].name);
+      console.log(country[0].flags.svg);
     });
+}
+
+refs.input.addEventListener('input', debounce(pushData, DEBOUNCE_DELAY));
+
+function pushData(event) {
+  console.log(event.target.value);
 }
