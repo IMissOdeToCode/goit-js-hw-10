def27c97ab511cbf55a3708205280fa1352a8abc
IMissOdeToCode <107809855+IMissOdeToCode@users.noreply.github.com>
2022-12-03 18:19:00 +0000

    Deploying to gh-pages from @ IMissOdeToCode/goit-js-hw-10@bcd3a19b3da7ebf464420e603bab522de51478d4 🚀

---
+++ index.9698e672.js
@@ -0,0 +1,2 @@
+fetch("https://restcountries.com/v2/name/poland?fields=name,capital,population,flags,languages").then((e=>e.json())).then((e=>{console.log(e[0].name)}));
+//# sourceMappingURL=index.9698e672.js.map
---
+++ index.fca62e0a.js
@@ -0,0 +1,2 @@
+fetch("https://restcountries.com/v2/name/poland?fields=name,capital,population,flags,languages").then((function(n){return n.json()})).then((function(n){console.log(n[0].name)}));
+//# sourceMappingURL=index.fca62e0a.js.map
