bcd3a19b3da7ebf464420e603bab522de51478d4
Denys Konopatkin <k.denis.aa@gmail.com>
2022-12-03 20:18:28 +0200

    start with api

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 import './css/styles.css';
 
 const DEBOUNCE_DELAY = 300;
+// const URL = 'https://restcountries.com/v2/name/poland';
+
+// prettier-ignore
+const URL = 'https://restcountries.com/v2/name/poland?fields=name,capital,population,flags,languages';
+
+fetchCountriesByName();
+
+function fetchCountriesByName() {
+  fetch(URL)
+    .then(response => {
+      return response.json();
+    })
+    .then(country => {
+      console.log(country[0].name);
+    });
+}
